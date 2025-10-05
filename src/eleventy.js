5e4b86b1aaa3aa0ff1e90e70ae5bbce7945a26da
ZeFish <francisfontaine@gmail.com>
2025-10-05 00:29:46 -0400

    Added default google font support

--- src/eleventy.js
+++ src/eleventy.js
@@ -16,6 +16,12 @@ module.exports = function (eleventyConfig, options = {}) {
       "node_modules/@zefish/standard/dist/standard.min.js": `${outputDir}/standard.min.js`,
     });
   }
+  // Shortcode to include Standard CSS and JS from local files
+  eleventyConfig.addShortcode("standard-fonts", function () {
+    return `<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wdth,wght@0,62..125,100..900;1,62..125,100..900&family=Fraunces:ital,opsz,wght,SOFT,WONK@0,9..144,100..900,0..100,0..1;1,9..144,100..900,0..100,0..1&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">`;
+  });
 
   // Shortcode to include Standard CSS and JS from local files
   eleventyConfig.addShortcode("meta-mobile", function () {
