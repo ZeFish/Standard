d9f59f5020a19239b9bcbec617cf4d63c300d355
ZeFish <francisfontaine@gmail.com>
2025-10-08 21:24:26 -0400

    awd

---
+++ src/eleventy/preprocessor.js
@@ -0,0 +1,30 @@
+export default function (eleventyConfig, options = {}) {
+  eleventyConfig.addPreprocessor("comment", "md", (data, content) => {
+    return content.replaceAll(/%%(.|\n|\s)*?%%/g, "");
+  });
+  eleventyConfig.addPreprocessor("commentCallouts", "md", (data, content) => {
+    return content.replace(/(^> \[!comments?\][^\n]*\n(?:^>.*\n?)*)/gm, "");
+  });
+  eleventyConfig.addPreprocessor("highlight", "md", (data, content) => {
+    return content.replace(/==(.+?)==/g, "<mark>$1</mark>");
+  });
+  eleventyConfig.addPreprocessor("fixDates", "md", (data, content) => {
+    const fixDateString = (value) => {
+      if (
+        typeof value === "string" &&
+        /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}/.test(value)
+      ) {
+        const date = new Date(value.replace(" ", "T"));
+        // If the date is invalid, return the original value to prevent errors
+        return isNaN(date.getTime()) ? value : date;
+      }
+      return value;
+    };
+
+    // Fix only known fields
+    if (data.created) data.created = fixDateString(data.created);
+    if (data.modified) data.modified = fixDateString(data.modified);
+
+    return content;
+  });
+}
