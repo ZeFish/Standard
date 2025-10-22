dd5e030d074e9d78fcc87df2eb45e631dddfa224
ZeFish <francisfontaine@gmail.com>
2025-10-22 13:33:58 -0400

    chore: release v0.11.2

--- dist/standard.js
+++ dist/standard.js
@@ -18,7 +18,7 @@
  *
  * Philosophy: Respect classic typography rules, but readability always wins.
  *
- * @version Standard Framework v0.11.0 | 2025-10-22T17:13:52.028Z
+ * @version Standard Framework v0.11.1 | 2025-10-22T17:33:49.584Z
  * @license MIT
  */
 
