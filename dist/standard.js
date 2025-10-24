ac2d4224aba4b7e75607978eb24978cda0e8d526
ZeFish <francisfontaine@gmail.com>
2025-10-23 22:24:10 -0400

    awd

--- dist/standard.js
+++ dist/standard.js
@@ -18,7 +18,7 @@
  *
  * Philosophy: Respect classic typography rules, but readability always wins.
  *
- * @version Standard Framework v0.11.25 | 2025-10-24T01:06:22.640Z
+ * @version Standard Framework v0.11.25
  * @license MIT
  */
 
