c88955c11b0efb54ad74cf24b3cf3bac06564c74
ZeFish <francisfontaine@gmail.com>
2025-10-27 12:25:12 -0400

    chore: release v0.13.8

--- dist/standard.comment.js
+++ dist/standard.comment.js
@@ -31,7 +31,7 @@
  * comments.attachFormHandler('#comment-form');
  *
  * @since 0.10.53
- * @version Standard Framework v0.13.7
+ * @version Standard Framework v0.13.8
  */
 
 class GitHubComments {
--- dist/standard.min.js
+++ dist/standard.min.js
@@ -13,7 +13,7 @@
  * - Custom events for extensibility
  * - Configurable selectors
  *
- * @version Standard Framework v0.13.7
+ * @version Standard Framework v0.13.8
  * @license MIT
  * @author Your Name
  */
