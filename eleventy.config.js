a19ef591b5aec2018f6add7f214d0ff1f2fc1620
Francis <francisfontaine@gmail.com>
2025-11-08 08:39:27 -0500

    theme change

--- eleventy.config.js
+++ eleventy.config.js
@@ -27,7 +27,7 @@ ai: {
 },
 
 */
-  eleventyConfig.addGlobalData("theme", "blueprint");
+  eleventyConfig.addGlobalData("theme", "kernel");
   eleventyConfig.addGlobalData("layout", "base");
   eleventyConfig.watchIgnores.add("_site/assets/**/*");
 
